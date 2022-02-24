2c24a0ffc28a7c31c7f41ab292d4eebd9155a71c
Lucas Vos <vos.lucas@outlook.com>
2022-02-24 22:25:38 +0100

    Initial commit
    Working codebase

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # kt82
 Key Topics - Assignment 8.2
+
+
+
+## Environment setup
+
+- python 3.x
+- pip
+- vscode 
+- git 
+
+I've collected (most) of the needed packages in requirements.txt. Install the packages with:
+
+    pip install -r requirements.txt
+
+
+## Notes
+Remark : I used pip to install opencv-python. This installs a precompiled CPU only version. This is sufficient for this project. GPU is not necessary.
+
+Dataset considerations
+- Manual curated dataset. Unbalanced
+- 15000 Non trafic ; 5000 Trafic
+
+Save_HOG_LBP code
+- It skips around 5000 non-traffic (number 10000-15000).. Potential bug : their code assumes that non-trafic images are listed before traffic images...
+
+Paper mentions that 20.000 images where used. And not about a skip of 5000 records.
+I think it is done to get a better balance between Non Traffic and Traffic.
+
+- The NonTrafic/ Trafic names are used in the pickle. Converted to Labels later on.
+
+Classifier
+- Paper mentions 80/20 split. 
+- Split into test/train is not explicitely made.
+- Scikit documentaion says that the `Probably=true` property of the classifier internally uses a 5 fold cross validation, hence the 
---
+++ classifier.py
@@ -0,0 +1,44 @@
+# import the necessary packages
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+import pickle
+import numpy as np
+
+# load the face embeddings
+print("[INFO] loading Road embeddings...")
+data = pickle.loads(open("embeddings.pickle", "rb").read())
+
+# encode the labels
+print("[INFO] encoding labels...")
+le = LabelEncoder()
+labels = le.fit_transform(data["names"])
+
+# LV  new code 24-02 
+# Documentation says that SVC( ... , probability=True) uses a 5 fold cross validation internally.
+# We could also  split the dataset in train and test manually? 
+# X_train, X_test, y_train, y_test = train_test_split(data["embeddings"], labels, test_size=0.2, random_state=42)
+# Paper is unclear how they did the score/recall calculations.
+
+
+print(np.array(data["embeddings"]).shape)
+# train the model used to accept the 2622-d embeddings of the face and
+# then produce the actual face recognition
+print("[INFO] training model...")
+clf = SVC(C=5, kernel="linear", probability=True)
+#clf = SVC(C=1, kernel="rbf" ,degree=3 , probability=True)
+clf.fit(data["embeddings"], labels)
+#clf.fit(X_train, y_train)
+print(clf.score(data["embeddings"], labels))
+
+
+
+# write the actual face recognition model to disk
+f = open("classifier.pickle", "wb")
+f.write(pickle.dumps(clf))
+f.close()
+
+# write the label encoder to disk
+f = open("le.pickle", "wb")
+f.write(pickle.dumps(le))
+f.close()
